d589775174669ea4ae683cd0832f1782ddf1b319
Ricardo Borillo <borillo@gmail.com>
2017-10-30 13:41:11 +0100

    Ajuste timeout test nightwatch

--- acceptance/tests/films.test.js
+++ acceptance/tests/films.test.js
@@ -2,9 +2,9 @@ export default {
   'Como starwarsadicto quiero listar todas las películas de la saga': (client) => {
     client = client
       .url('http://localhost:3000')
-      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('body', 2000)
       .assert.title('React App')
-      .waitForElementVisible('.App', 10000)
+      .waitForElementVisible('.App', 2000)
       .assert.containsText('.App', 'The Force Awakens')      
       .assert.containsText('.film:nth-child(1) .episode', '1')
       .assert.containsText('.film:nth-child(1) .title', 'The Phantom Menace')
