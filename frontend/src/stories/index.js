beb8ec9c4ea390324e80368e954151cf5eacf184
Ricardo Borillo <borillo@gmail.com>
2017-12-19 17:38:42 +0100

    Added info addon

--- frontend/src/stories/index.js
+++ frontend/src/stories/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withInfo } from '@storybook/addon-info';
 
 import About from '../About';
 import Films from '../Films';
@@ -11,5 +12,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 storiesOf('ReactWars', module)
-  .add('About', () => <About />)
-  .add('Films', () => <Films />);
+  .add('About', withInfo({
+    text: 'Main docs for this component can we specified here', 
+  })(() => <About />))
+  .add('Films', withInfo()(() => <Films />));
